7efebaee5375d3d0babd86041d0220f87bb98a79
amarsmann1224 <70251359+amarsmann1224@users.noreply.github.com>
2020-09-27 17:45:59 -0700

    Add files via upload

---
+++ DNApalind.py
@@ -0,0 +1,9 @@
+s=input("Enter DNA string: ")
+comp={"G":"C","C":"G","A":"T","T":"A"}
+slist=list(s)
+rs=""
+for i in slist:
+    rs=comp[i] +rs
+if rs==s:
+    print("DNA Palindrome")
+else:print("Not DNA Palindrome")
